46e8af7712a909bd831a4004f9e2661e9238f8c7
oltdaniel <53529846+oltdaniel@users.noreply.github.com>
2020-10-28 17:55:59 +0100

    do some package stuff

--- tasks/main.go
+++ tasks/main.go
@@ -7,29 +7,37 @@ import (
 	"github.com/onatm/clockwerk"
 )
 
+// Register the general task informations.
 type Task struct {
 	Name     string
 	Interval time.Duration
 	Function func()
 }
 
+// Wrap the task function in a warpper.
 func (t Task) Run() {
 	fmt.Printf("Running: '%v'.\n", t.Name)
 	t.Function()
 }
 
+// Keep track of the registered tracks.
 var registeredTasks = []Task{}
+
+// The scheduler instance.
 var clockwerkInstance = clockwerk.New()
 
+// Register all known tasks in the system.
 func RegisterAllTasks() {
 	RegisterTask(messageTask)
 }
 
+// Register a single task in the scheduler and the registered Task array.
 func RegisterTask(t Task) {
 	registeredTasks = append(registeredTasks, t)
 	clockwerkInstance.Every(t.Interval).Do(t)
 }
 
+// Start the scheduler.
 func Start() {
 	clockwerkInstance.Start()
 }
